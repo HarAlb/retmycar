9c3f98bdcffc0149f07cd7740d2bb20e34faa64a
Albert <haralb01@gmail.com>
2025-06-16 17:26:33 +0400

    FOr build

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -23,7 +23,7 @@ class AuthRequest extends FormRequest
     {
         return [
             'email' => 'required|string|max:255',
-            'password' => 'required|string'
+            'password' => 'required|string',
         ];
     }
 }
