b26c7e872c2377d7fdcbc0fe40007db177bf212d
Albert <haralb01@gmail.com>
2025-07-30 13:50:18 +0400

    FIX

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Route::get('/clear-cache', function () {
    \Illuminate\Support\Facades\Artisan::call('cache:clear');
+   \Illuminate\Support\Facades\Artisan::call('route:clear');
+   \Illuminate\Support\Facades\Artisan::call('config:clear');
 });
 
 Route::get('/generate-doc', function () {
